07dc244d0f0e5080beef4c13981065ac623e327a
Roberto Domínguez <201902@CALASANZSALAMANCA.local>
2020-12-15 11:37:53 +0100

    vc

---
+++ ej9Promesas.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", manejarEventosSubmit, false);
+
+function verAnimales(){
+    return new Promise(
+        (resolve, reject) => {
+            let request = new XMLHttpRequest;
+            request.open('GET', '', true);
+        }
+    )
+}
